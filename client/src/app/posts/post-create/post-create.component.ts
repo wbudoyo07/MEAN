3a606a2cea1be9b19f2124b12672f20af404ff60
William Budoyo <williambu07@gmail.com>
2019-01-28 18:37:27 -0500

    added Data injection

--- client/src/app/posts/post-create/post-create.component.ts
+++ client/src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter} from '@angular/core';
+
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl : './post-create.component.html'
+  templateUrl : './post-create.component.html',
+  styleUrls : ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue ='';
-  newPost = 'NO CONTENT';
+  enteredContent ='';
+  enteredTitle = '';
+ postsCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  constructor(public postsService: PostsService) {}
+  onAddPost(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
 
-   this.newPost = this.enteredValue;
+
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
---
+++ client/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Post } from "../post.model";
+import { PostService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl :'./post.list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit {
+  // posts = [
+  //   {
+  //     title: 'First Post',
+  //     content: "This is the first post's content",
+  //   },
+  //   {
+  //     title: 'Second Post',
+  //     content: "This is the second post's content",
+  //   },
+  //   {
+  //     title: 'Third Post',
+  //     content: "This is the third post's content",
+  //   },
+  // ];
+  @Input() posts: Post[] = [];
+
+
+  constructor(public postService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
+}
